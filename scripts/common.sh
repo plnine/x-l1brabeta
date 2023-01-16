00db527b11c487f5c1a9d920b3332d34ca28bff2
plnine <122791309+plnine@users.noreply.github.com>
2023-01-16 17:29:46 +0300

    Create common.sh

---
+++ scripts/common.sh
@@ -0,0 +1,27 @@
+NC="\e[0m"           # no color
+CYAN="\e[1m\e[1;96m" # cyan color
+RED="\e[1m\e[1;91m" # red color
+
+function printLogo {
+  bash <(curl -s https://github.com/plnine/x-l1bra/blob/main/logo.sh)
+}
+
+function printLine {
+  echo "---------------------------------------------------------------------------------------"
+}
+
+function printCyan {
+  echo -e "${CYAN}${1}${NC}"
+}
+
+function printRed {
+  echo -e "${RED}${1}${NC}"
+}
+
+function addToPath {
+  source $HOME/.bash_profile
+  PATH_EXIST=$(grep ${1} $HOME/.bash_profile)
+  if [ -z "$PATH_EXIST" ]; then
+    echo "export PATH=$PATH:${1}" >>$HOME/.bash_profile
+  fi
+}
