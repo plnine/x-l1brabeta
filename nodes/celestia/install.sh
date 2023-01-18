e5e9a8cdd276bb2f840bcfd85a8f031179b9ee00
plnine <pl9-fund@pm.me>
2023-01-18 19:22:26 +0300

    DZHAGERR

--- nodes/celestia/install.sh
+++ nodes/celestia/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #X-l1bra
 clear && source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/common.sh)
-
-
 printLogo
 source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/celestia/asciicelestia.sh)
 printRed  =======================================================================
@@ -22,33 +20,6 @@ sudo apt install -y make clang pkg-config libssl-dev build-essential git gcc lz4
 #################################################
 printGreen "Installation completed." && sleep 1
 
-printYellow "3.Installing go........" && sleep 1
-#################################################
-cd $HOME  > /dev/null 2>&1
-ver="1.18.3"  > /dev/null 2>&1
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"  > /dev/null 2>&1
-sudo rm -rf /usr/local/go  > /dev/null 2>&1
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"  > /dev/null 2>&1
-rm "go$ver.linux-amd64.tar.gz"  > /dev/null 2>&1
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile  > /dev/null 2>&1
-source $HOME/.bash_profile  > /dev/null 2>&1
-go version
-#################################################
-printGreen "Completed."
-
-printYellow "4.Download and install binary........"
-#################################################
-cd $HOME || return > /dev/null 2>&1
-rm -rf celestia-app > /dev/null 2>&1
-git clone https://github.com/celestiaorg/celestia-app.git > /dev/null 2>&1
-cd celestia-app || return > /dev/null 2>&1
-git checkout v0.11.0 > /dev/null 2>&1
-make install > /dev/null 2>&1
-printCyan "Celestia version"
-celestia-appd version # 0.11.0
-#################################################
-printGreen "Completed."
-
 printYellow "5.Set variables........" && sleep 1
 #################################################
 CHAIN_ID="mocha"
@@ -64,38 +35,58 @@ echo -e "IDENTITY:           ${CYAN}$IDENTITY${NC}"
 #################################################
 printGreen "Completed." && sleep 1
 
+printYellow "3.Installing go........" && sleep 1
+#################################################
+if ! [ -x "$(command -v go)" ]; then
+  source <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/go_install.sh")
+  source .bash_profile
+fi
+
+echo "$(go version)"
+#################################################
+printGreen "Completed." && sleep 1
+
+printYellow "4.Download and install binary........"
+#################################################
+cd $HOME || return
+rm -rf celestia-app
+git clone https://github.com/celestiaorg/celestia-app.git
+cd celestia-app || return
+git checkout v0.11.0
+make install
+celestia-appd version # 0.11.0
+
 printYellow "6.Initialize the node........" && sleep 1
 #################################################
-celestia-appd config keyring-backend test > /dev/null 2>&1
-celestia-appd config chain-id $CHAIN_ID > /dev/null 2>&1
-celestia-appd init "$NODE_MONIKER" --chain-id $CHAIN_ID > /dev/null 2>&1
+celestia-appd config keyring-backend test
+celestia-appd config chain-id $CHAIN_ID
+celestia-appd init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -s https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/genesis.json > $HOME/.celestia-app/config/genesis.json > /dev/null 2>&1
-curl -s https://snapshots3-testnet.nodejumper.io/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json > /dev/null 2>&1
+curl -s https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/genesis.json > $HOME/.celestia-app/config/genesis.json
+curl -s https://snapshots3-testnet.nodejumper.io/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json
 #################################################
 printGreen "Completed." && sleep 1
 
 printYellow "7.Adding seeds and peers........" && sleep 1
 #################################################
-SEEDS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/seeds.txt | tr -d '\n') > /dev/null 2>&1
-PEERS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/peers.txt | tr -d '\n') > /dev/null 2>&1
-sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.celestia-app/config/config.toml > /dev/null 2>&1
+SEEDS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/seeds.txt | tr -d '\n')
+PEERS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/peers.txt | tr -d '\n')
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.celestia-app/config/config.toml
 #################################################
 printGreen "Completed." && sleep 1
-
 
 printYellow "8.Setting up pruning........" && sleep 1
 #################################################
-PRUNING_INTERVAL=$(shuf -n1 -e 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97) > /dev/null 2>&1
-sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.celestia-app/config/app.toml > /dev/null 2>&1
-sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.celestia-app/config/app.toml > /dev/null 2>&1
-sed -i 's|^pruning-interval *=.*|pruning-interval = "'$PRUNING_INTERVAL'"|g' $HOME/.celestia-app/config/app.toml > /dev/null 2>&1
-sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' $HOME/.celestia-app/config/app.toml > /dev/null 2>&1
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001utia"|g' $HOME/.celestia-app/config/app.toml > /dev/null 2>&1
-sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.celestia-app/config/config.toml > /dev/null 2>&1
+PRUNING_INTERVAL=$(shuf -n1 -e 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.celestia-app/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.celestia-app/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "'$PRUNING_INTERVAL'"|g' $HOME/.celestia-app/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' $HOME/.celestia-app/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001utia"|g' $HOME/.celestia-app/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.celestia-app/config/config.toml
 #################################################
 printGreen "Completed." && sleep 1
-
 
 printYellow "9.Create a service file........" && sleep 1
 #################################################
@@ -115,23 +106,15 @@ EOF
 #################################################
 printGreen "Completed." && sleep 1
 
-celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app --keep-addr-book > /dev/null 2>&1
+celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app --keep-addr-book
 
 SNAP_NAME=$(curl -s https://snapshots3-testnet.nodejumper.io/celestia-testnet/ | egrep -o ">mocha.*\.tar.lz4" | tr -d ">")
-
-printYellow "9.Download snapshots........" && sleep 1
-#################################################
 curl https://snapshots3-testnet.nodejumper.io/celestia-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.celestia-app
-#################################################
-printGreen "Completed." && sleep 1
 
 sudo systemctl daemon-reload
 sudo systemctl enable celestia-appd
 sudo systemctl start celestia-appd
 
-cd
-
-printGreen "Installation completed" && sleep 1
 
 printRed  =============================================================================== 
 echo -e "Check logs:                ${CYAN} sudo journalctl -u celestia-appd -f --no-hostname -o cat ${NC}"
@@ -140,4 +123,3 @@ echo -e "Add New Wallet:            ${CYAN} celestia-appd keys add wallet${NC}"
 echo -e "Recover Existing Wallet:   ${CYAN} celestia-appd keys add wallet --recover${NC}"
 echo -e "X-l1bra:                   ${CYAN} https://t.me/xl1bra${NC}"
 printRed  =============================================================================== 
- 
